97087284a97ea27b5ea4a25fec3a3bfd46aa017b
Frank Mastronardi <mastronardif@gmail.com>
2017-01-21 20:01:42 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,40 @@
+var express = require("express");
+var app = express();
+var router = express.Router();
+var path = __dirname + '/views/';
+
+app.use(express.static(__dirname + '/public'));
+
+router.use(function (req,res,next) {
+  console.log("/" + req.method);
+  next();
+});
+
+router.get("/",function(req,res){
+  res.sendFile(path + "index.html");
+});
+
+router.get("/taxi",function(req,res){
+  res.sendFile(path + "taxi.html");
+});
+router.get("/grocery",function(req,res){
+  res.sendFile(path + "grocery.html");
+});
+
+router.get("/about",function(req,res){
+  res.sendFile(path + "about.html");
+});
+
+router.get("/contact",function(req,res){
+  res.sendFile(path + "contact.html");
+});
+
+app.use("/",router);
+
+app.use("*",function(req,res){
+  res.sendFile(path + "404.html");
+});
+
+app.listen(3000,function(){
+  console.log("Live at Port 3000");
+});
