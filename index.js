d4c8d186734ed8196e70a46f51038bf6821161a4
Frank Mastronardi <mastronardif@gmail.com>
2017-03-06 15:08:32 -0500

    updated index.js

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('You have reached the default node.js application at index.js! [defaultdocument sample]');
+    res.end('FxM application at index.js! [defaultdocument sample]');
 }).listen(process.env.PORT); 
